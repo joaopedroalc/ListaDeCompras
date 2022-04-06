20f6a08cfa0ab3bfe6e1e43b44d3846bdfecfa89
seap <joaopedro.ufma@gmail.com>
2022-04-06 16:27:58 -0300

    ainda com virgulas

--- main.js
+++ main.js
@@ -3,6 +3,114 @@ let close = document.querySelector('.close')
 let itensSelecionados = document.querySelector('.itens-selecionados')
 let divContent = document.querySelector('.itens-selecionados div')
 
+mercearia = [
+  "Arroz",
+  "Feijão",
+  "Açucar",
+  "Sal",
+  "Ovos",
+  "Vinagre",
+  "Macarrão",
+  "Óleo",
+  "Mostarda",
+  "Ketchup",
+  "Maionese",
+  "Trigo",
+  "Fermento",
+  "Pipoca",
+  "Adoçante",
+]
+
+  frios = [
+    "Leite",
+    "Iorgute",
+    "Manteiga",
+    "Presunto",
+    "Queijo",
+    "Frita",
+    "Hamburguer",
+  ]
+
+  matinais = [
+    "Café",
+    "Chá",
+    "Achocolatado",
+    "Cereais",
+    "Geleia",
+    "Biscoito",
+    "Pães",
+    "Pão de Forma",
+  ]
+
+  acougue = [
+    "Bife",
+    "Moída",
+    "Fígado",
+    "Costela",
+    "Porco",
+    "Calabresa",
+    "Toscana",
+    "Frango",
+    "Peixe",
+  ]
+  hortiFruti = [
+    "Alho",
+    "Alface",
+    "Batata",
+    "Beterraba",
+    "Cenoura",
+    "Cebola",
+    "Chuchu",
+    "Couve",
+    "Pimentão",
+    "Salsa",
+    "Cebolinha",
+    "Coentro",
+    "Vargem",
+    "Maçã",
+    "Uva",
+    "Banana",
+    "Morango",
+    "Mamão",
+    "Maracujá",
+    "Abacate",
+    "Abacaxi",
+  ]
+
+function adicionarProdutos(produtos) {
+  const itens = produtos.map(produto => {
+    return `
+    <div class="item">
+      <label class="label" for="${produto}">${produto}</label>
+      <input type="checkbox" class="checkbox" id="${produto}" value="${produto}">
+    </div>
+    `;
+  })
+  return itens
+}
+
+const todosMercearia = adicionarProdutos(mercearia)
+const todosFrios = adicionarProdutos(frios)
+const todosMatinais = adicionarProdutos(matinais)
+const todosAcougue = adicionarProdutos(acougue)
+const todosHortiFruti = adicionarProdutos(hortiFruti)
+
+const containerMercearia = document.querySelector('.containerMercearia')
+containerMercearia.innerHTML += todosMercearia
+
+const containerFrios = document.querySelector('.containerFrios')
+containerFrios.innerHTML += todosFrios
+
+const containerMatinais = document.querySelector('.containerMatinais')
+containerMatinais.innerHTML += todosMatinais
+
+const containerAçougue = document.querySelector('.containerAçougue')
+containerAçougue.innerHTML += todosAcougue
+
+const containerHortiFruti = document.querySelector('.containerHortiFruti')
+containerHortiFruti.innerHTML += todosHortiFruti
+
+
 carrinho.onclick = () => {
   itensSelecionados.classList.add('active')
   close.classList.add('on')
@@ -13,27 +121,27 @@ close.onclick = () => {
   close.classList.remove('on')
 }
 
-if(document.readyState == 'loading'){
-  document.addEventListener('DOMContentLoaded',ready)
-}else{
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready)
+} else {
   ready();
 }
 
-function ready(){
+function ready() {
   let lixeira = document.getElementsByClassName('lixeira')
-  for(let i = 0; i < lixeira.length;i++) {
+  for (let i = 0; i < lixeira.length; i++) {
     let button = lixeira[i]
     button.addEventListener('click', removeCartItem)
   }
 
   let checkboxs = document.getElementsByClassName('checkbox')
-  for(let i = 0;i<checkboxs.length;i++) {
+  for (let i = 0; i < checkboxs.length; i++) {
     let button = checkboxs[i]
-    button.addEventListener('click',addCartClicked)
+    button.addEventListener('click', addCartClicked)
   }
-} 
+}
 
-function addCartClicked(event){
+function addCartClicked(event) {
   let button = event.target
   let item = button.parentElement
   let label = item.getElementsByClassName('label')[0].innerHTML
@@ -45,10 +153,10 @@ function addCartClicked(event){
 
 let checkBoxs = document.querySelectorAll('.checkbox')
 
-function addProductToCart(label){
+function addProductToCart(label) {
   let divItens = document.createElement('div')
   divItens.classList.add('resultado')
-  
+
   let content = `
   <span class="adicionado">${label}</span>
   <input type="number" name="numero" min="0" max="99" class="num" value="1">
@@ -61,8 +169,9 @@ function addProductToCart(label){
       if (check == false) {
         el.target.classList.add('block')
       }
-    })})
-  
+    })
+  })
+
   divItens.innerHTML = content;
   divContent.appendChild(divItens)
   divItens.getElementsByClassName('lixeira')[0].addEventListener('click', removeCartItem)
@@ -70,10 +179,10 @@ function addProductToCart(label){
 
 function removeCartItem(event) {
   let r = confirm("Tem certeza ? Se você remover esse item não poderá adiciona-lô novamente!");
-  if (r==true){
-      let buttonClicked = event.target;
-      buttonClicked.parentElement.remove()
-    }
+  if (r == true) {
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.remove()
+  }
 }
 
 function addTudo() {
